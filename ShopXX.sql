0ff5d9d8c2a3503445fd99de59bad593e0596f74
Jin <106419088+nnnancy0123@users.noreply.github.com>
2022-09-30 20:53:48 +0900

    u
    u

--- ShopXX.sql
+++ ShopXX.sql
@@ -1,5 +1,5 @@
 create table shop_product (
-  id serial not null 
+  id  serial not null 
   , name character varying(50) not null
   , simple_desc character varying(300)
   , price integer not null
@@ -17,3 +17,5 @@ drop table shop_product;
 select * from shop_product;
 
 insert into shop_product (id,name,simple_desc,price,inventory,image,show_flg,del_flg,date_created,date_modified)values(1,'jin','â¬à§Ç¢',100,20,'C:\image\1.jpg',1,1,'2022/07/16','2022-09-13')
+
+select * from shop_product WHERE name::text like 'çgíÉÉ~ÉãÉN';
